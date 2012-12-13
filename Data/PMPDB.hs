d01e3c2f718ba1b4d6c0bde5c8fdb07322b0aeed
K. Ikegami <liquid@eidos.ic.i.u-tokyo.ac.jp>
2012-12-13 11:10:16 +0900

    Move common definiton to Data.PMPDB.

---
+++ Data/PMPDB.hs
@@ -0,0 +1,16 @@
+module Data.PMPDB where
+
+
+import Data.Word
+import Data.Time
+import qualified Data.ByteString.Lazy as BL
+
+
+data PMPDB = PMPString     [BL.ByteString]
+           | PMPWord8      [Word8]
+           | PMPWord16     [Word16]
+           | PMPWord32     [Word32]
+           | PMPWord64     [Word64]
+           | PMPStringList [[BL.ByteString]]
+           | PMPDateTime   [UTCTime]
+           deriving (Show)
--- Data/PMPDB/Reader.hs
+++ Data/PMPDB/Reader.hs
@@ -1,11 +1,12 @@
 module Data.PMPDB.Reader where
 
 
+import Data.PMPDB
 import Data.Word
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Binary.Get as G
-import Data.Binary.IEEE754
-import Data.Time.Clock
+-- import Data.Binary.IEEE754
+-- import Data.Time
 
 
 getConditional :: (Show a) => (G.Get a) -> (a -> Bool) -> G.Get a
@@ -17,15 +18,6 @@ getConditional parser cond = do
 -- TODO: incorrect CSV parsing for string containing ',' itself
 getCSV :: G.Get [BL.ByteString]
 getCSV = G.getLazyByteStringNul >>= return . BL.split 0x2c
-
-
-data PMPDB = PMPString     [BL.ByteString]
-           | PMPWord8      [Word8]
-           | PMPWord16     [Word16]
-           | PMPWord32     [Word32]
-           | PMPWord64     [Word64]
-           | PMPStringList [[BL.ByteString]]
-           | PMPDateTime   [UTCTime]
 
 
 parseList :: (G.Get a) -> Int -> G.Get [a]
