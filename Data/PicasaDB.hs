e63a349659e2d1a397f1d88b100d6bf8717fd342
K. Ikegami <liquid@eidos.ic.i.u-tokyo.ac.jp>
2012-12-22 14:07:53 +0900

    Separated into modules.

--- Data/PicasaDB.hs
+++ Data/PicasaDB.hs
@@ -3,6 +3,7 @@ module Data.PicasaDB where
 
 import Data.Word
 import Data.Time
+import Data.Maybe
 import qualified Data.Text.Lazy as TL
 
 
@@ -15,6 +16,7 @@ data PMPDB = PMPString     [TL.Text]
            | PMPDateTime   [UTCTime]
            deriving (Show)
 
+
 listPMPDB :: PMPDB -> IO ()
 listPMPDB (PMPString     xs) = mapM_ (putStrLn . TL.unpack) xs
 listPMPDB (PMPWord8      xs) = mapM_ (putStrLn . show) xs
@@ -23,3 +25,69 @@ listPMPDB (PMPWord32     xs) = mapM_ (putStrLn . show) xs
 listPMPDB (PMPWord64     xs) = mapM_ (putStrLn . show) xs
 listPMPDB (PMPStringList xs) = (putStr . TL.unpack . TL.unlines . map TL.unwords) xs
 listPMPDB (PMPDateTime   xs) = mapM_ (putStrLn . show) xs
+
+
+data FileType = Empty
+              | Directory
+              | Jpeg
+              | Gif
+              | Wbmp
+              | Photoshop
+              | Avi -- ambiguous, avi is only the container
+              | Mpeg4 -- ??
+              | H264 -- ??
+              | Tiff
+              | Png
+              | NikonRaw -- I'm not sure about Canon or other vendor's RAW.
+              | Xml
+              | UnExpected --  detecting error
+              deriving (Show, Eq)
+
+
+intToFileTypeValid :: Integral a => a -> Maybe FileType
+intToFileTypeValid    0 = Just Empty
+intToFileTypeValid 0xe9 = Just Empty
+intToFileTypeValid 0x01 = Just Directory
+intToFileTypeValid 0x02 = Just Jpeg
+intToFileTypeValid 0x03 = Just Gif
+intToFileTypeValid 0x05 = Just Directory
+intToFileTypeValid 0x06 = Just Wbmp
+intToFileTypeValid 0x07 = Just Photoshop
+intToFileTypeValid 0x08 = Just Avi
+intToFileTypeValid 0x09 = Just Mpeg4
+intToFileTypeValid 0x0a = Just H264
+intToFileTypeValid 0x0d = Just Tiff
+intToFileTypeValid 0x0e = Just Png
+intToFileTypeValid 0x12 = Just NikonRaw
+intToFileTypeValid 0x1e = Just Xml
+intToFileTypeValid    _ = Nothing
+
+
+intToFileType :: Integral a => a -> FileType
+intToFileType = maybe UnExpected id . intToFileTypeValid
+
+
+data ThumbIndexEntry = ThumbIndexEntry
+                       { path :: FilePath
+                       , ctime :: UTCTime
+                       , mtime :: UTCTime
+                       , infox :: String
+                       , ftype :: FileType
+                       , infoy :: String
+                       , valid :: Int
+                       , directory :: Int
+                       }
+                     deriving (Show)
+
+
+showTSV :: ThumbIndexEntry -> String
+showTSV x = tail $ concatMap ('\t':) $ map (flip ($) x) [path,
+                                                         show . ctime,
+                                                         show . mtime,
+                                                         infox,
+                                                         show . ftype, 
+                                                         infoy,
+                                                         show . valid,
+                                                         show . directory]
+
+{- END -}
--- thumbIndex.hs
+++ thumbIndex.hs
@@ -1,7 +1,7 @@
 import Data.Word
 import Data.DateTime
+import Data.PicasaDB
 import Data.PicasaDB.Reader
-import Numeric
 import Control.Monad ((>=>), (<=<))
 import qualified Data.Binary.Get as G
 import qualified Data.ByteString as B
@@ -17,58 +17,10 @@ import Text.Printf
 -- g = \y x -> flip showHex (' ' : y) x
 --   = flip showHex . (' ' :)
 
-hexDump :: BL.ByteString -> String
-hexDump = foldr f "" . BL.unpack
-  where f = flip $ flip showHex' . (' ' :)
-        showHex' x | x < 16    = ('0' :) . showHex x
-                   | otherwise = showHex x
-
-
-fromHiResTime :: Integral a => a -> DateTime
-fromHiResTime = flip addSeconds origin . flip div 10000000 . fromIntegral
-  where
-    origin = fromGregorian 1601 1 1 0 0 0
-
-
-getListUntil :: G.Get a -> G.Get [a]
-getListUntil f = do
-  empty <- G.isEmpty
-  case empty of
-    True  -> return  []
-    False -> do
-      x <- f
-      return . (x :) =<< getListUntil f
-
-
-getHeader :: G.Get Int
-getHeader = ensureMagic >> G.getWord32le >>= return . fromIntegral
-  where
-    ensureMagic = getConditional G.getWord32be (== 0x66664640)
-
-
-type Entry = (String, DateTime, DateTime, String, Int, String, Int, Int)
-
-
-getEntry :: G.Get Entry
-getEntry = do
-  path <- return . TL.unpack =<< getUtf8LazyTextNul
-  ctime <- return . fromHiResTime =<< G.getWord64le
-  mtime <- return . fromHiResTime =<< G.getWord64le
-  infx <- return . hexDump =<< G.getLazyByteString 4
-  tval <- return . fromIntegral =<< G.getWord8
-  infy <- return . hexDump =<< G.getLazyByteString 4
-  valid <- G.getWord8
-  idir <- return . fromIntegral =<< G.getWord32le
-  if valid == 0 && idir /= -1 then fail "invalid with valid idir"
-    else return (path, ctime, mtime, infx, tval, infy, fromIntegral valid, idir)
-
-
-readThumbsIndex :: FilePath -> IO [Entry]
-readThumbsIndex = return . G.runGet (getHeader >> getListUntil getEntry) <=< BL.readFile
+readThumbsIndex :: FilePath -> IO [ThumbIndexEntry]
+readThumbsIndex = return . G.runGet (getThumbIndexDB) <=< BL.readFile
 
 
 main = do
   args <- getArgs
-  mapM_ (readThumbsIndex >=> mapM_ (putStrLn . f)) args
-  where
-    f = \(s, t, u, v, w, x, y, z) -> s ++ concatMap ('\t':) [show t, show u, v, show w, x, show y, show z]
+  mapM_ (readThumbsIndex >=> mapM_ (putStrLn . showTSV)) args
